56199b3b0fd6be9c87b9050fabdfaab0e607b0d3
jab1897 <138234117+jab1897@users.noreply.github.com>
2025-08-13 10:01:03 -0500

    Delete web/src/Components directory

--- web/src/Components/ShareButton.tsx
+++
@@ -1,7 +0,0 @@
-export default function ShareButton(){
-  async function copy(){
-    await navigator.clipboard.writeText(window.location.href);
-    alert("Link copied!");
-  }
-  return <button className="btn" onClick={copy}>Share</button>;
-}
