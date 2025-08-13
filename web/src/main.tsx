9240d1850a3c6439f0b15c30cdacf54e5585a2f5
jab1897 <138234117+jab1897@users.noreply.github.com>
2025-08-13 09:51:04 -0500

    Update main.tsx

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -4,7 +4,7 @@ import { RouterProvider } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { router } from "./routes";
 import "./styles/global.css";
-import Nav from "./components/Nav";
+import Nav from "@/components/Nav";
 
 const qc = new QueryClient();
 
